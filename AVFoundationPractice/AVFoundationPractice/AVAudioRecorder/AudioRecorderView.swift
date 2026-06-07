db938317c273735e8c60be98a787648af0d35958
이은지 <eklee2057@naver.com>
2026-06-07 18:52:05 +0900

    feat: recorder 테스트 view 추가

---
+++ AVFoundationPractice/AVFoundationPractice/AVAudioRecorder/AudioRecorderView.swift
@@ -0,0 +1,44 @@
+//
+//  AudioRecorderView.swift
+//  AVFoundationPractice
+//
+//  Created by 이은지 on 6/7/26.
+//
+
+import SwiftUI
+
+struct AudioRecorderView: View {
+    @StateObject private var audioRecorder = AudioRecorder()
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            Text("Audio Recorder")
+            
+            Button {
+                switch audioRecorder.isRecording {
+                case true:
+                    audioRecorder.stopRecording()
+                case false:
+                    audioRecorder.startRecording()
+                }
+            } label: {
+                Image(systemName: audioRecorder.isRecording ? "stop.circle.fill" : "record.circle")
+                    .font(.largeTitle)
+                    .foregroudStyle(audioRecorder.isRecording ? .red : .primary)
+            }
+            
+            Button {
+                switch audioRecorder.isPlaying {
+                case true:
+                    audioRecorder.stopPlaying()
+                case false:
+                    audioRecorder.playRecording()
+                }
+            } label: {
+                Image(systemName: audioRecorder.isPlaying ? "stop.fill" : "play.fill")
+                    .font(.largeTitle)
+            }
+        }
+        .padding()
+    }
+}
